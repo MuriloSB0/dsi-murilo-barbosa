ef22c59b9c2598bcbdc66f4d2467699951febe74
Murilo Barbosa <murilosbarbosa.2009@gmail.com>
2026-05-04 09:27:26 -0300

    aula06

--- aula05/atribuicao.java
+++ aula05/atribuicao.java
@@ -1,3 +1,57 @@
 public class atribuicao { 
-    
+    public static void main(String[] args) {
+    int x = 5; 
+    System.out.println(x);
+    //Define um valor para o item (neste caso o valor de 'x' foi definido como 5)
+
+    int y = 5;
+    y += 3;
+    System.out.println(y);
+    //Define o item como a soma de seu valor original ('5') ao valor do inserido (neste caso o '3')
+
+    int z = 5;
+    z -= 3;
+    System.out.println(z);
+    //Define o item como seu valor original subtraido pelo valor colocado (3)
+
+    int w = 5;
+    w *= 3;
+    System.out.println(w);
+    //Define o item como seu valor original multiplicado pelo valor colocado
+
+    double v = 5;
+    v /= 3;
+    System.out.println(v);
+    //Define o item como seu valor original dividido pelo valor colocado
+
+    int u = 5;
+    u %= 3;
+    System.out.println(u);
+    //Define o item como
+
+    int t = 5;
+    t &= 3;
+    System.out.println(t);
+    //Define o item como
+
+    int s = 5;
+    s |= 3;
+    System.out.println(s);
+    //Define o item como
+
+    int r = 5;
+    r ^= 3;
+    System.out.println(r);
+    //Define o item como
+
+    int q = 5;
+    q >>= 3;
+    System.out.println(q);
+    //Define o item como
+
+    int p = 5;
+    p <<= 3;
+    System.out.println(p);
+    //Define o item como
+  }
 }
